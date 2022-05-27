e1483bd483088f7ca2e478ea1148bb1f8995454a
Mahbub Noyon <mahbubnoyon506@gmail.com>
2022-05-27 14:29:53 +0600

    Again pushing for solving issue

--- index.js
+++ index.js
@@ -8,8 +8,26 @@ const port = process.env.PORT || 5000;
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
-app.use(cors({origin:"*"}));
-app.use(express.json());
+
+// app.use(cors({origin:"*"}));
+// app.use(express.json());
+
+const corsConfig = {
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+    }
+    app.use(cors(corsConfig))
+    app.options("*", cors(corsConfig))
+    app.use(express.json())
+    app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
+    next()
+    })
+
+
+
 
 function jwtVerify(req, res, next) {
     const authHeaders = req.headers.authorization;
